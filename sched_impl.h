adee88ae09f59999c782ec6009404d4766ec44a4
jgonz521336 <60712917+jgonz521336@users.noreply.github.com>
2021-05-03 21:54:54 -0500

    Update sched_impl.h

--- sched_impl.h
+++ sched_impl.h
@@ -3,10 +3,16 @@
 
 struct thread_info {
 	/*...Fill this in...*/
+	list_t* queue;
+        list_elem_t* queueData;
+        sem_t runWorker;
 };
 
 struct sched_queue {
 	/*...Fill this in...*/
+	list_elem_t* currentWorker;
+        list_elem_t* nextWorker;
+	list_t* list;
 };
 
 #endif /* __SCHED_IMPL__H__ */

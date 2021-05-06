c8f868c11ae3d57843f86fd1179f29f2226a1568
jgonz521336 <60712917+jgonz521336@users.noreply.github.com>
2021-05-05 20:09:34 -0500

    Update sched_impl.h

--- sched_impl.h
+++ sched_impl.h
@@ -1,6 +1,11 @@
 #ifndef	__SCHED_IMPL__H__
 #define	__SCHED_IMPL__H__
 
+#include "list.h"
+#include "scheduler.h"
+#include <pthread.h>
+#include <semaphore.h>
+
 struct thread_info {
 	/*...Fill this in...*/
 	list_t* queue;
